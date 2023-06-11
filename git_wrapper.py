dcee03e22065605f8f53429aafd96fb77633bab7
Otto White <otto.white@bidfx.com>
2023-06-11 20:03:00 +0100

    Refactor: unify indentation scheme

--- git_wrapper.py
+++ git_wrapper.py
@@ -1,18 +1,18 @@
 import os
 
 from command_wrapper import runCommand
-from text_processing import getRepoNameFromGitUrl, printTitle
+from text_processing import getRepoNameFromGitUrl
 
 def synchronizeWithRepo(repoHttpUrl):
-    repoName = getRepoNameFromGitUrl(repoHttpUrl)
-    pullOrCloneRepo(repoName, repoHttpUrl)
+  repoName = getRepoNameFromGitUrl(repoHttpUrl)
+  pullOrCloneRepo(repoName, repoHttpUrl)
 
-    return repoName
+  return repoName
 
 def pullOrCloneRepo(repoFileName, repoHttpUrl):
-    if os.path.isdir(repoFileName):
-        print("1) Repo already exists, pulling")
-        runCommand(f'cd {repoFileName} && git pull && cd ..', printOutput=True)
-    else:
-        print("1) Repo doesn\'t exist, cloning")
-        runCommand(f"git clone {repoHttpUrl}", printOutput=True)
+  if os.path.isdir(repoFileName):
+    print("1) Repo already exists, pulling")
+    runCommand(f'cd {repoFileName} && git pull && cd ..', printOutput=True)
+  else:
+    print("1) Repo doesn\'t exist, cloning")
+    runCommand(f"git clone {repoHttpUrl}", printOutput=True)
--- main.py
+++ main.py
@@ -4,36 +4,31 @@ import sys
 
 from git_wrapper import synchronizeWithRepo
 from filesystem_wrapper import hardLinkConfigFile
-from text_processing import (
-    getRepoNameFromGitUrl,
-    parseConfigDirectoryCsvLine,
-    printTitle
-)
+from text_processing import parseConfigDirectoryCsvLine, printTitle
 
 def main(repoHttpUrl):
-    printTitle(f"Getting latest changes from {repoHttpUrl}")
-    repoName = synchronizeWithRepo(repoHttpUrl)
+  printTitle(f"Getting latest changes from {repoHttpUrl}")
+  repoName = synchronizeWithRepo(repoHttpUrl)
 
-    configDirectoryCsvPath = os.path.join(repoName, "config_directory.csv")
+  configDirectoryCsvPath = os.path.join(repoName, "config_directory.csv")
 
-    with open(configDirectoryCsvPath) as configDirectoryCsvFile:
-        for configDirectoryCsvLine in configDirectoryCsvFile:
+  with open(configDirectoryCsvPath) as configDirectoryCsvFile:
+    for configDirectoryCsvLine in configDirectoryCsvFile:
 
-            (configFileSrcName, 
-             configFileDstPath, 
-             configFileRequired) = parseConfigDirectoryCsvLine(configDirectoryCsvLine)
+      (configFileSrcName, 
+       configFileDstPath, 
+       configFileRequired) = parseConfigDirectoryCsvLine(configDirectoryCsvLine)
 
-            configFileSrcPath = os.path.join(repoName, "config_files", configFileSrcName)
+      configFileSrcPath = os.path.join(repoName, "config_files", configFileSrcName)
 
-            if configFileRequired:
-              printTitle(configFileSrcName)
-
-              hardLinkConfigFile(configFileSrcPath, configFileDstPath)
-            else:
-              printTitle(f"{configFileSrcName} (skipping)")
+      if configFileRequired:
+        printTitle(configFileSrcName)
+        hardLinkConfigFile(configFileSrcPath, configFileDstPath)
+      else:
+        printTitle(f"{configFileSrcName} (skipping)")
 
 if __name__ == '__main__':
-    main(sys.argv[1])
+  main(sys.argv[1])
 
 # TODO: Add windows from WSL support (With copying perhaps)
 # TODO: Add --set-repo flag and write the value to a file
